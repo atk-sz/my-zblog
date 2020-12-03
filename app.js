82260731e4e725087d851cda4a8099338e46fce0
atk-sz <65167734+atk-sz@users.noreply.github.com>
2020-12-03 15:16:28 +0530

    added images dir to both css and home

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const express = require('express'),
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public/"));
+app.use(express.static(__dirname + "/images/"));
 
 app.get('/', (req, res) => {
     res.render('home')
